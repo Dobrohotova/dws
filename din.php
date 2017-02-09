42564a27184b8cfa8084e7d7e94d56036bd9f774
Dobrohotova <irina.dobrohotova@gmail.com>
2017-02-09 10:15:55 +0100

    First class basic php

---
+++ din.php
@@ -0,0 +1,49 @@
+<?php
+
+$a = 20;
+$b = 10;
+$c = 7;
+
+// if ($a > $b && $a > $c) {
+// 	echo $a;
+// } else if ($b > $a && $b > $c) {
+// 	echo $b;
+// } else {
+// 	echo $c;
+// }
+
+
+if ($a > $b) {
+	if ($a > $c){
+		echo $a;
+	} else {
+		echo $c;
+	}
+} else {
+	if ($b > $c){
+		echo $b;
+	} else {
+		echo $c;
+	}
+}
+
+
+
+
+
+
+
+$site_varijabli = array($a, $b, $c); 
+
+
+
+for($i = 0; $i < count($site_varijabli); $i++){
+	if (is_numeric($site_varijabli[$i])){
+		echo $site_varijabli[$i];
+	} else {
+		echo "Внеси број!";
+	}
+}
+// }
+
+?>
